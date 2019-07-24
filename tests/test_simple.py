8a23b8edbcfb1d19ccbf2c87b64afdb2da8b8291
remorses <beats.by.morse@gmail.com>
2019-07-24 12:25:20 +0200

    added AsynConf

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,11 +1,26 @@
 
-from xconfig import Config
-
+from xconfig import Config, AsyncConfig
+import pytest
 
 def test_simple():
     c = Config('./test.yaml')
+    c.delete()
     c.write('tests.test.cosa', 69)
     c.write('tests.test.altro', 69)
+    for i in range(5):
+        c.push('tests.test.array', i)
+    print()
+    print(c)
+    assert True
+
+@pytest.mark.asyncio
+async def test_async():
+    c = await AsyncConfig('./test.yaml')
+    await c.delete()
+    await c.write('tests.test.cosa', 69)
+    await c.write('tests.test.altro', 69)
+    for i in range(5):
+        await c.push('tests.test.array', i)
     print()
     print(c)
     assert True
--- xconfig/__init__.py
+++ xconfig/__init__.py
@@ -1,6 +1,7 @@
 import json
 import yaml
 import os.path
+import aiofiles
 
 class Dumper(yaml.Dumper):
     def increase_indent(self, flow=False, indentless=False):
@@ -35,7 +36,7 @@ class Config:
     def push(self, prop, value):
         content = self.content
         for part in prop.split('.')[:-1]:
-            content[part] = {}
+            content[part] = content[part] if part in content else {}
             content = content[part]
         field = prop.split('.')[-1]
         if field in content:
@@ -58,7 +59,72 @@ class Config:
         return self.content[name]
     
     def delete(self):
-        os.remove(self.filename)
+        try:
+            os.remove(self.filename)
+        except Exception:
+            pass
+    
+    def __repr__(self):
+        return 'Config(filename="' + self.filename + '", data= ' + json.dumps(self.content, indent=4, default=str) + ')\n'
+
+async def AsyncConfig(filename, default={}):
+    self = _AsyncConfig()
+    if not os.path.exists(filename):
+        self.content = default
+        self.default = default
+        # with open(filename, 'w') as f:
+        #     f.write(default)
+    else:
+        async with aiofiles.open(filename,) as f:
+            s = await f.read()
+            data = yaml.safe_load(s) or {}
+            self.content = {**default, **data}
+
+    self.filename = filename
+    return self
+
+class _AsyncConfig:
+
+    async def write(self, prop, value):
+        content = self.content
+        for part in prop.split('.')[:-1]:
+            content[part] = content[part] if part in content else {}
+            content = content[part]
+        content[prop.split('.')[-1]] = value
+        async with aiofiles.open(self.filename, 'w') as f:
+            data = dumps(self.content, )
+            await f.write(data)
+
+    async def push(self, prop, value):
+        content = self.content
+        for part in prop.split('.')[:-1]:
+            content[part] = content[part] if part in content else {}
+            content = content[part]
+        field = prop.split('.')[-1]
+        if field in content:
+            if isinstance(content[field], (list, tuple)):
+                content[field].append(value)
+            else:
+                content[field] = [value]    
+        else:
+            content[field] = [value]
+        async with aiofiles.open(self.filename, 'w') as f:
+            data = dumps(self.content, )
+            await f.write(data)
+
+    def __getitem__(self, name):
+        filename = self.filename
+        if os.path.exists(filename):
+            with open(filename,) as f:
+                data = yaml.safe_load(f) or {}
+                self.content = {**self.default, **data, **self.content}
+        return self.content[name]
+    
+    async def delete(self):
+        try:
+            os.remove(self.filename)
+        except Exception:
+            pass
     
     def __repr__(self):
         return 'Config(filename="' + self.filename + '", data= ' + json.dumps(self.content, indent=4, default=str) + ')\n'
